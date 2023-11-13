f452f1d08eb176f3f32ed18df17bef06937701a8
trleminhduy@gmail.com <trleminhduy@gmail.com>
2023-11-13 14:05:59 +0700

    chinhphuong

--- project.cpp
+++ project.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 using namespace std;
+bool ktraSCP (int num){
+    int sqrtNum = sqrt(num);
+    return sqrtNum *sqrtNum==num;
+}
 int main (){
     int n;
     cout<<"Nhap n";
     cin>>n;
+ int number1 =20;
+    if(ktraSCP(number1)){
+        cout<<"Day la so chinh phuong";
+    }else{
+        cout<<"Day kh phai";
+    }
     
 }
